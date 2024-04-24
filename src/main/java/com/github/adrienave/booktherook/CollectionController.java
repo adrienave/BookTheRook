b985379f0a1c6b12183a84f001e2d0be3c4dfa5d
adrienave <adrien.avenia@gmail.com>
2024-04-24 21:53:12 +0200

    Add simple tree view, with inputs to add new folder

---
+++ src/main/java/com/github/adrienave/booktherook/CollectionController.java
@@ -0,0 +1,31 @@
+package com.github.adrienave.booktherook;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CollectionController implements Initializable {
+    @FXML
+    private TreeView<String> collectionTree;
+    @FXML
+    private TextField newFolderNameInput;
+
+    private TreeItem<String> collectionRoot;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        collectionRoot = new TreeItem<>("Folders");
+        collectionTree.setRoot(collectionRoot);
+    }
+
+    @FXML
+    protected void createFolder() {
+        TreeItem<String> newFolder = new TreeItem<>(newFolderNameInput.getText());
+        collectionRoot.getChildren().add(newFolder);
+    }
+}
