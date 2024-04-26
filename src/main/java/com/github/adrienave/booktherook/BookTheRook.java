115617222cf75296cfac08116841bfb366684160
adrienave <adrien.avenia@gmail.com>
2024-04-26 16:29:19 +0200

    Display folder names as bold and prepend game names with rook fa icon
    Covers #9

--- src/main/java/com/github/adrienave/booktherook/BookTheRook.java
+++ src/main/java/com/github/adrienave/booktherook/BookTheRook.java
@@ -7,11 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class BookTheRook extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BookTheRook.class.getResource("collection-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("collection-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         CollectionController controller = fxmlLoader.getController();
         scene.setOnKeyPressed(keyEvent -> {
@@ -25,6 +26,7 @@ public class BookTheRook extends Application {
                 }
             }
         });
+        scene.getStylesheets().add(Objects.requireNonNull(this.getClass().getResource("collection-view.css")).toExternalForm());
         stage.setTitle("Book The Rook - Chess Game Collection Manager");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/github/adrienave/booktherook/javafx/CollectionTreeCellImpl.java
+++ src/main/java/com/github/adrienave/booktherook/javafx/CollectionTreeCellImpl.java
@@ -2,10 +2,14 @@ package com.github.adrienave.booktherook.javafx;
 
 import com.github.adrienave.booktherook.controller.CollectionController;
 import com.github.adrienave.booktherook.model.GameRecord;
+import javafx.scene.Node;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeCell;
+import org.kordamp.ikonli.javafx.FontIcon;
 import lombok.RequiredArgsConstructor;
+
+import static org.kordamp.ikonli.fontawesome5.FontAwesomeSolid.CHESS_ROOK;
 
 @RequiredArgsConstructor
 public class CollectionTreeCellImpl extends TreeCell<Object> {
@@ -21,10 +25,29 @@ public class CollectionTreeCellImpl extends TreeCell<Object> {
             setGraphic(null);
             setContextMenu(null);
         } else {
+            setGraphic(computeIcon(item));
+            getStyleClass().removeAll("folder-item", "game-item");
+            getStyleClass().add(computeStyleClass(item));
             setContextMenu(generateContextMenu(item));
             setOnMouseClicked(mouseEvent -> handleClick(item));
             setText(item.toString());
         }
+    }
+
+    private Node computeIcon(Object item) {
+        if (item instanceof GameRecord) {
+            return new FontIcon(CHESS_ROOK);
+        }
+        return null;
+    }
+
+    private String computeStyleClass(Object item) {
+        if (item instanceof String) {
+            return "folder-item";
+        } else if (item instanceof GameRecord) {
+            return "game-item";
+        }
+        return "";
     }
 
     private ContextMenu generateContextMenu(Object item) {
